695536f213d930a31fc33b9dfa065ade03c5560c
Edouard Heitzmann <edouardheitzmann@gmail.com>
2025-07-04 07:24:13 -0600

    Add fast adjacency, make old one asymmetric

--- src/scores.py
+++ src/scores.py
@@ -89,7 +89,17 @@ def make_adjacency_matrix(profile: PreferenceProfile,candidate_to_index):
                         candidate_to_index[ranking[i+1]] #this seems like an error, should be i+1
                         ] += ballot.weight
             
-    adjacencies = adjacencies + adjacencies.T
+    #adjacencies = adjacencies + adjacencies.T
+    return adjacencies
+
+def fast_adj(profile: PreferenceProfile):
+    candidate_to_index = {candidate: i for i, candidate in enumerate(profile.candidates)} #this is the canonical ordering of candidates
+    adjacencies = np.zeros((len(profile.candidates), len(profile.candidates)))
+    for bal in profile.ballots:
+        good_bal, w = bal_to_tuple(bal, candidate_to_index)
+        if len(good_bal)>1:
+            for i in range(len(good_bal) - 1):
+                adjacencies[good_bal[i], good_bal[i+1]] += w
     return adjacencies
 
 def fast_cut_score(profile: PreferenceProfile, partition32, adjacencies): #this is not quite giving the same number as cut_score... oh well.
