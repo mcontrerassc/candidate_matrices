f372213230e079a00b31c6cf5265e51dd4d351fd
mcontrerassc <mcontrerascatalan25@amherst.edu>
2025-07-15 10:43:59 -0400

    Add louvain for signed graphs

--- src/clustering.py
+++ src/clustering.py
@@ -5,8 +5,16 @@ from votekit.utils import mentions
 from votekit.matrices import matrix_heatmap, boost_matrix
 import matplotlib.pyplot as plt
 import numpy as np
-from cdlib import algorithms, evaluation
+from typing import Optional, Union, Tuple
+from scipy import sparse
 import networkx as nx
+from sknetwork.utils.check import get_probs
+from sknetwork.utils.format import get_adjacency
+from sknetwork.utils.membership import get_membership
+from cdlib import algorithms, evaluation
+from collections import defaultdict
+from networkx.algorithms import community
+import sknetwork as skn
 
 
 def create_graph_louvain_bm(boost: np.ndarray):
@@ -43,6 +51,20 @@ def louvain_partition_from_graph(G: nx.Graph, profile: PreferenceProfile, resolu
         partition.append(list_cands)
     return partition, modularity_score
 
+
+def second_modularity(G: nx.Graph, resolution):
+        model = skn.clustering.Louvain(resolution=resolution)   
+        labels = model.fit_predict(G)
+            # convert label_dict to our standard format for a clustering: a list of elections.
+        k = max(label_dict.values()) + 1 # number of clusters
+        C = [{} for _ in range(k)]
+        for ballot, weight in election.items():
+            truncated_ballot = ballot[:trunc]
+            label = label_dict[truncated_ballot]
+            C[label][ballot] = weight
+        # remove empty clusters (which come from labels only assigned to uncast ballots)
+        C = [c for c in C if sum(c.values()) > 0]  
+
 def split_matrix(A):
     A_pos = np.where(A > 0, A, 0)
     A_neg = np.where(A < 0, A, 0)
@@ -67,8 +89,6 @@ def modularity_from_B(B, assignment, pm='pos', m=None):
         m = B.sum()
     return Q / m if m != 0 else 0.0
 
-import numpy as np
-from collections import defaultdict
 
 def louvain_directed(A, pm='pos', max_iter=100):
     n = A.shape[0]
@@ -115,8 +135,6 @@ def louvain_directed(A, pm='pos', max_iter=100):
 
     return assignment
 
-import numpy as np
-from collections import defaultdict
 
 def louvain_directed_hybrid(A, max_iter=100):
     n = A.shape[0]
@@ -175,3 +193,113 @@ def louvain_directed_hybrid(A, max_iter=100):
             break
 
     return assignment
+
+
+
+# method borrowed from scikit-network, modified for our Q^-
+def get_modularity_neg(input_matrix: Union[sparse.csr_matrix, np.ndarray], labels: np.ndarray,
+                   labels_col: Optional[np.ndarray] = None, weights: str = 'degree',
+                   resolution: float = 1, return_all: bool = False) -> Union[float, Tuple[float, float, float]]:
+    """Modularity of a clustering.
+
+    The modularity of a clustering is
+
+    :math:`Q = \\dfrac{1}{w} \\sum_{i,j}\\left(A_{ij} - \\gamma \\dfrac{w_iw_j}{w}\\right)\\delta_{c_i,c_j}`
+    for graphs,
+
+    :math:`Q = \\dfrac{1}{w} \\sum_{i,j}\\left(A_{ij} - \\gamma \\dfrac{d^+_id^-_j}{w}\\right)\\delta_{c_i,c_j}`
+    for directed graphs,
+
+    where
+
+    * :math:`c_i` is the cluster of node :math:`i`,\n
+    * :math:`w_i` is the weight of node :math:`i`,\n
+    * :math:`w^+_i, w^-_i` are the out-weight, in-weight of node :math:`i` (for directed graphs),\n
+    * :math:`w = 1^TA1` is the total weight,\n
+    * :math:`\\delta` is the Kronecker symbol,\n
+    * :math:`\\gamma \\ge 0` is the resolution parameter.
+
+    Parameters
+    ----------
+    input_matrix :
+        Adjacency matrix or biadjacency matrix of the graph.
+    labels :
+        Labels of nodes.
+    labels_col :
+        Labels of column nodes (for bipartite graphs).
+    weights :
+        Weighting of nodes (``'degree'`` (default) or ``'uniform'``).
+    resolution:
+        Resolution parameter (default = 1).
+    return_all:
+        If ``True``, return modularity, fit, diversity.
+
+    Returns
+    -------
+    modularity : float
+    fit: float, optional
+    diversity: float, optional
+
+    Example
+    -------
+    >>> from sknetwork.clustering import get_modularity
+    >>> from sknetwork.data import house
+    >>> adjacency = house()
+    >>> labels = np.array([0, 0, 1, 1, 0])
+    >>> float(np.round(get_modularity(adjacency, labels), 2))
+    0.11
+    """
+    adjacency, bipartite = get_adjacency(input_matrix.astype(float))
+
+    if bipartite:
+        if labels_col is None:
+            raise ValueError('For bipartite graphs, you must specify the labels of both rows and columns.')
+        else:
+            labels = np.hstack((labels, labels_col))
+
+    if len(labels) != adjacency.shape[0]:
+        raise ValueError('Dimension mismatch between labels and input matrix.')
+
+    probs_row = get_probs(weights, adjacency)
+    probs_col = get_probs(weights, adjacency.T)
+    membership = 1 - get_membership(labels).astype(float).toarray()
+
+    fit = membership.T.dot(adjacency.dot(membership)).diagonal().sum() / adjacency.data.sum()
+    div = membership.T.dot(probs_col).dot(membership.T.dot(probs_row))
+    mod = fit - resolution * div
+    if return_all:
+        return mod, fit, div
+    else:
+        return mod
+    
+def create_graph_louvain_signed(boost: np.ndarray, pos: bool):
+    n = boost.shape[0]
+    if pos == True:
+        boost = np.where(boost > 0, boost, 0)
+    else:
+        boost = np.where(boost < 0, -boost, 0)
+    # build a graph
+    G = nx.Graph()
+    for i in range(n):
+        #for j in range(i+1, n):
+        for j in range(n):
+            G.add_edge(i, j, weight = boost[i, j])
+    return G
+
+# convert labels to a partition
+def labels_to_partitions(labels, candidates):
+    clusters = defaultdict(list)
+    for label, candidate in zip(labels, candidates):
+        clusters[label].append(candidate)
+    # return as list of lists sorted by cluster label (optional)
+    return [clusters[k] for k in sorted(clusters.keys())]
+
+# resolution kept as 1 
+def louvain_partition_signed_graph(G, profile, resolution=1):
+    adj = sparse.csr_matrix(nx.to_scipy_sparse_array(G, weight='weight', format='csr'))
+    model = skn.clustering.Louvain(resolution=resolution)   
+    labels = model.fit_predict(adj)
+    partition = labels_to_partitions(labels, list(profile.candidates))
+    modularity = get_modularity_neg(adj, labels)
+    return partition, modularity
+
